4419e7e509caffb4859927c45b828648d76bc8a4
Павел Бурков <pasha.burkov.59@gmail.com>
2019-06-13 11:41:35 +0400

    доделал кнопки и форму 2

--- Хлебокомбинат/Хлебокомбинат/Form2.Designer.cs
+++ Хлебокомбинат/Хлебокомбинат/Form2.Designer.cs
@@ -33,14 +33,21 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -85,17 +92,6 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Учебная практика - Бурков Павел Андреевич (ИСП-922)";
             // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(602, 71);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 76);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Добавить запись";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label4
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
@@ -129,49 +125,118 @@
             this.label6.TabIndex = 11;
             this.label6.Text = "Кол-во хлеба:";
             // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox1.Location = new System.Drawing.Point(173, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 12;
-            // 
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox2.Location = new System.Drawing.Point(173, 108);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(101, 20);
+            this.textBox2.Size = new System.Drawing.Size(123, 20);
             this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
             // 
             // textBox3
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox3.Location = new System.Drawing.Point(173, 137);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(123, 20);
             this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox3_KeyPress);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(173, 76);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(123, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 163);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(344, 190);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Дата поставки";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Кол-во хлеба";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Цена поставки ";
+            this.Column3.Name = "Column3";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(28, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 46);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Добавить запись";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(153, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 46);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Удалить запись";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(280, 371);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 46);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Сохранить таблицу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Name = "Form2";
             this.Text = "Хлебокомбинат";
+            this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,12 +248,18 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Хлебокомбинат/Хлебокомбинат/Form2.cs
+++ Хлебокомбинат/Хлебокомбинат/Form2.cs
@@ -7,14 +7,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Хлебокомбинат
 {
     public partial class Form2 : Form
     {
+        private string myDirectory = string.Empty;
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox2.Text == "" && textBox3.Text == "")
+            {
+                MessageBox.Show("Заполните все поля.", "Ошибка.");
+            }
+            else
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = dateTimePicker1.Text;
+                dataGridView1.Rows[n].Cells[1].Value = textBox2.Text; 
+                dataGridView1.Rows[n].Cells[2].Value = textBox3.Text;
+            }
+            
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.MaxLength = 4;
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+                return;
+            }
+            String text = textBox3.Text;
+
+            if (text == "0" && e.KeyChar != 8)
+                e.Handled = true;
+
+            if (e.KeyChar == 48 && textBox3.SelectionStart == 0 && text != "")
+                e.Handled = true;
+
+            {
+                if (textBox3.Text.Length == 0)
+                    if (e.KeyChar == '0') e.Handled = true;
+            }
+        }
+
+        private void TextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+                return;
+            }
+            String text = textBox3.Text;
+
+            if (text == "0" && e.KeyChar != 8)
+                e.Handled = true;
+
+            if (e.KeyChar == 48 && textBox3.SelectionStart == 0 && text != "")
+                e.Handled = true;
+
+            {
+                if (textBox3.Text.Length == 0)
+                    if (e.KeyChar == '0') e.Handled = true;
+            }
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.MaxLength = 4;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для удаления.", "Ошибка.");
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet XLEB = new DataSet();
+                DataTable Tablica = new DataTable
+                {
+                    TableName = "Все поставки"
+                };
+                Tablica.Columns.Add("Дата поставки");
+                Tablica.Columns.Add("Кол-во хлеба");
+                Tablica.Columns.Add("Цена поставки");
+                XLEB.Tables.Add(Tablica);
+
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    DataRow row = XLEB.Tables["Все поставки"].NewRow();
+                    row["Дата поставки"] = r.Cells[0].Value;
+                    row["Кол-во хлеба"] = r.Cells[1].Value;
+                    row["Цена поставки"] = r.Cells[2].Value;
+                    XLEB.Tables["Все поставки"].Rows.Add(row);
+                }
+                XLEB.WriteXml(myDirectory + @"\XLEB.xml");
+                XLEB = new DataSet();
+                MessageBox.Show("XML файл успешно сохранен.", "Выполнено.");
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно сохранить XML файл.", "Ошибка.");
+            }
         }
     }
 }
