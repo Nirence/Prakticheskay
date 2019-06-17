1c5e918dc2cf29fc5f0d6ddea661b70b0c4bcc49
Павел Бурков <pasha.burkov.59@gmail.com>
2019-06-17 17:34:04 +0400

    Сделал график

--- Хлебокомбинат/Хлебокомбинат/Form3.Designer.cs
+++ Хлебокомбинат/Хлебокомбинат/Form3.Designer.cs
@@ -29,19 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -94,42 +96,10 @@
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 95);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 71);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(344, 168);
+            this.dataGridView1.Size = new System.Drawing.Size(344, 274);
             this.dataGridView1.TabIndex = 9;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button1.Location = new System.Drawing.Point(246, 269);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 52);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Построить график";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button2.Location = new System.Drawing.Point(12, 269);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 52);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Загрузить таблицу";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button3.Location = new System.Drawing.Point(126, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(114, 52);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Удалить таблицу";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Column1
             // 
@@ -152,14 +122,28 @@
             this.Column3.ReadOnly = true;
             this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(445, 71);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Количество";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(343, 274);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
@@ -169,6 +153,7 @@
             this.Text = "Хлебокомбинат";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,11 +166,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Хлебокомбинат/Хлебокомбинат/Form3.cs
+++ Хлебокомбинат/Хлебокомбинат/Form3.cs
@@ -17,8 +17,34 @@ namespace Хлебокомбинат
         public Form3()
         {
             InitializeComponent();
+            Shown += new EventHandler(Dll);
+            Shown += new EventHandler(Dlc);
         }
+        private void Dll(object sender, EventArgs e)
+        {
+            if (File.Exists(myDirectory + @"\XLEB.xml"))
+            {
+                DataSet Tablica = new DataSet();
+                Tablica.ReadXml(myDirectory + @"\XLEB.xml");
 
+                foreach (DataRow item in Tablica.Tables["Все поставки"].Rows)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[0].Value = item["Дата поставки"];
+                    dataGridView1.Rows[n].Cells[1].Value = item["Кол-во хлеба"];
+                    dataGridView1.Rows[n].Cells[2].Value = item["Цена поставки"];
+                }
+            }
+            else
+            {
+                MessageBox.Show("XML файл не найден.", "Ошибка.");
+            }
+        }
+        private void Dlc(object sender, EventArgs e)
+        {
+            this.chart1.Series["Количество"].Points.AddXY("07.04.2019", 35);
+            this.chart1.Series["Количество"].Points.AddXY("14.04.2019", 18);
+        }
         private void Button2_Click(object sender, EventArgs e)
         {
             if (dataGridView1.Rows.Count > 1)
