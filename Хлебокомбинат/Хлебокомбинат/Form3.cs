19246243a758a41205f99e34148a1813e626df9b
Павел Бурков <pasha.burkov.59@gmail.com>
2019-06-18 15:30:37 +0400

    Готовая программа

--- Хлебокомбинат/Хлебокомбинат/Form3.cs
+++ Хлебокомбинат/Хлебокомбинат/Form3.cs
@@ -45,46 +45,5 @@ namespace Хлебокомбинат
             this.chart1.Series["Количество"].Points.AddXY("07.04.2019", 35);
             this.chart1.Series["Количество"].Points.AddXY("14.04.2019", 18);
         }
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            if (dataGridView1.Rows.Count > 1)
-            {
-                MessageBox.Show("Очистите поле перед загрузкой нового файла.", "Ошибка.");
-            }
-            else
-            {
-                if (File.Exists(myDirectory + @"\XLEB.xml"))
-                {
-                    DataSet Tablica = new DataSet();
-                    Tablica.ReadXml(myDirectory + @"\XLEB.xml");
-
-                    foreach (DataRow item in Tablica.Tables["Все поставки"].Rows)
-                    {
-                        int n = dataGridView1.Rows.Add();
-                        dataGridView1.Rows[n].Cells[0].Value = item["Дата поставки"];
-                        dataGridView1.Rows[n].Cells[1].Value = item["Кол-во хлеба"];
-                        dataGridView1.Rows[n].Cells[2].Value = item["Цена поставки"];
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("XML файл не найден.", "Ошибка.");
-                }
-            }
-        }
-
-        private void Button3_Click(object sender, EventArgs e)
-        {
-            {
-                if (dataGridView1.Rows.Count > 0)
-                {
-                    dataGridView1.Rows.Clear();
-                }
-                else
-                {
-                    MessageBox.Show("Таблица пустая.", "Ошибка.");
-                }
-            }
-        }
     }
 }
